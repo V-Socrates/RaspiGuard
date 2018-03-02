7afe03ba44853e3f387a3cca5ecca25fe6f1d584
n01147386 <31747980+n01147386@users.noreply.github.com>
2018-03-02 01:55:54 -0500

    from static to dynamic

---
+++ Software/RaspiGuardWeb/statistics.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>RaspiGuard | Statistics</title>
+  <!-- Bootstrap core CSS-->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
+</head>
+
+<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+  <!-- Navigation-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <a class="navbar-brand" href="index.php">RaspiGuard</a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
+          <a class="nav-link" href="index.php">
+            <i class="fa fa-fw fa-dashboard"></i>
+            <span class="nav-link-text">Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">
+          <a class="nav-link" href="statistics.php">
+            <i class="fa fa-fw fa-area-chart"></i>
+            <span class="nav-link-text">Statistics</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
+          <a class="nav-link" href="activitylog.php">
+            <i class="fa fa-fw fa-table"></i>
+            <span class="nav-link-text">Activity Log</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
+          <a class="nav-link" href="manageaccount.php">
+            <i class="fa fa-fw fa-wrench"></i>
+            <span class="nav-link-text">Manage Account</span>
+          </a>
+        </li>
+      </ul>
+      <ul class="navbar-nav sidenav-toggler">
+        <li class="nav-item">
+          <a class="nav-link text-center" id="sidenavToggler">
+            <i class="fa fa-fw fa-angle-left"></i>
+          </a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+            <i class="fa fa-fw fa-sign-out"></i>Logout</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="content-wrapper">
+    <div class="container-fluid">
+      <!-- Breadcrumbs-->
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="#">Dashboard</a>
+        </li>
+        <li class="breadcrumb-item active">Statistics</li>
+      </ol>
+      <!-- Area Chart Example-->
+      <div class="card mb-3">
+        <div class="card-header">
+          <i class="fa fa-area-chart"></i> Moisture Levels</div>
+        <div class="card-body">
+          <canvas id="myAreaChart" width="100%" height="30"></canvas>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8">
+          <!-- Example Bar Chart Card-->
+          <div class="card mb-3">
+            <div class="card-header">
+              <i class="fa fa-bar-chart"></i> Activities per Month</div>
+            <div class="card-body">
+              <canvas id="myBarChart" width="100" height="50"></canvas>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <!-- Example Pie Chart Card-->
+          <div class="card mb-3">
+            <div class="card-header">
+              <i class="fa fa-pie-chart"></i> Average Light Per Day</div>
+            <div class="card-body">
+              <canvas id="myPieChart" width="100%" height="100"></canvas>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.container-fluid-->
+    <!-- /.content-wrapper-->
+    <footer class="sticky-footer">
+      <div class="container">
+        <div class="text-center">
+          <small>Copyright © Your Website 2018</small>
+        </div>
+      </div>
+    </footer>
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fa fa-angle-up"></i>
+    </a>
+    <!-- Logout Modal-->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+            <a class="btn btn-primary" href="login.html">Logout</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Page level plugin JavaScript-->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin.min.js"></script>
+	
+    <!-- Custom scripts for this page-->
+    <script src="js/sb-admin-charts.js"></script>
+	<?php
+			$servername = "165.227.44.224";
+			$username = "user";
+			$password = "user";
+			$database = "raspiguard";
+			define('newline',"<br>\n");
+
+			// Create connection
+			$conn = mysqli_connect($servername, $username, $password, $database);
+
+			
+			$sql="SELECT SUM(DATE(datetime) BETWEEN '2017-11-01' AND '2018-11-30') AS Nov,
+	   SUM(DATE(datetime) BETWEEN '2017-12-01' AND '2018-12-31') AS Dets,
+	   SUM(DATE(datetime) BETWEEN '2018-01-01' AND '2018-01-31') AS Jan, 
+       SUM(DATE(datetime) BETWEEN '2018-02-01' AND '2018-02-28') AS Feb, 
+       SUM(DATE(datetime) BETWEEN '2018-03-01' AND '2018-03-31') AS Mar,
+       SUM(DATE(datetime) BETWEEN '2018-04-01' AND '2018-04-30') AS Apr
+		FROM `activitylog`"; //NEED TO FIX THIS WITH LOGGED IN USERNAME DATA 
+
+			$result = mysqli_query($conn, $sql);
+			
+			
+			
+			
+			
+			if (mysqli_num_rows($result) > 0) {
+				
+				echo "<script>" ; 
+				echo "var ctx = document.getElementById('myBarChart');"  ; 
+				 
+				$row = mysqli_fetch_row($result);
+				 				
+				$barChartData=sprintf("var myLineChart = new Chart(ctx, {type: 'bar',data: {labels: ['November', 'December', 'January', 'February', 'March', 'April'],datasets: [{label: 'Activities',backgroundColor: 'rgba(2,117,216,1)',borderColor: 'rgba(2,117,216,1)',data: [%s, %s, %s, %s, %s, %s],}],},options: {scales: {xAxes: [{time: {unit: 'month'},gridLines: {display: false},ticks: {maxTicksLimit: 6}}],yAxes: [{ticks: {min: 0,max: 1000,maxTicksLimit: 5},gridLines: {display: true}}],},legend: {display: false}}});", 
+						   $row[0], $row[1], $row[2], $row[3], $row[4], $row[5]);
+				   echo $barChartData;
+
+				echo "</script>" ; 
+				
+				
+				
+				
+
+				
+			}
+			
+			
+			
+			
+?>
+	
+  </div>
+</body>
+
+</html>
+
+
